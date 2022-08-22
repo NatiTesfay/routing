32844403f2805290281ea96e9fc0748904b8d9c9
netanel tesfay <netaneltes66@gmailgit config --global user.email you@example.com.com>
2022-08-22 19:44:12 +0300

    Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import{Header,Main,Footer} from './coponents/pages/index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+function Routing({root}) {
+  switch (root) {
+    case "Header":
+      return <Header/>
+      case "Main":
+      return <Main/>
+      case "Footer":
+        return <Footer/>
+    default:
+      break;
+  }
+};
+export default Routing;
+        //   <Router>
+        //    <div className='container'>
+        //     <nav>
+        //       <ul>
+        //         <li>
+        //           <link to='./Home'>Home</link>
+        //           <link to='./About'>about</link>
+        //           <link to='./ConectUs'>ContactUs</link>
+        //         </li>
+        //       </ul>
+        //     </nav>
+         
+        //    <Switch>  
+        //    <Route path='./ConectUs'component={ConectUs}/>
+        //    <Route path='./About'component={About}/>
+        //    <Route path='./ConectUs'component={Home}/>
+        //    </Switch>
+        //  </div>
+        //  </Router>
+
